f00232a2cd43bb0a9c8c45c01565be22ffca907c
Arvind-NITT <arvindmali5768@gmail.com>
2024-05-09 23:09:10 +0530

    Day 21

---
+++ Day21/RequestTrackerAppSolution/RequestTrackerApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace RequestTrackerApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Day21/RequestTrackerAppSolution/SQLQuery1.sql
@@ -0,0 +1,3 @@
+use dbEmployeeTrackerCF
+
+select * from Employees;
